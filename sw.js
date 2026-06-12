5d867b8d5fd4d5a841ea7850d45c8f82e392d947
code-house10 <107010368+code-house10@users.noreply.github.com>
2026-06-12 22:30:58 +0300

    c15

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'jungle-movie-dialogue-practice-v10';
-const ASSETS = ['./', './index.html', './style.css?v=dialogue-practice-v10', './app.js?v=dialogue-practice-v10', './vocab-data.js?v=dialogue-practice-v10'];
+const CACHE_NAME = 'jungle-movie-dialogue-style-v11';
+const ASSETS = ['./', './index.html', './style.css?v=dialogue-style-v11', './app.js?v=dialogue-style-v11', './vocab-data.js?v=dialogue-style-v11'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
